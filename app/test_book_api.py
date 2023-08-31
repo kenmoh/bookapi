8db2df62e2ccbe074f8b67792e861e438d4e4e5f
kenmoh <kenneth.aremoh@gmail.com>
2023-08-31 15:38:54 +0100

    test update

--- app/test_book_api.py
+++ app/test_book_api.py
@@ -26,7 +26,7 @@ update_data = {
 review_data = {
     "author": "Lee Sammy",
     "comment": "Testing",
-    "rating": 3
+    "rating": 5
 }
 
 
@@ -46,7 +46,7 @@ def test_add_movie():
 
 
 def test_update_movie():
-    response = client.put(f'{BASE_URL}/3', json=update_data)
+    response = client.put(f'{BASE_URL}/6', json=update_data)
     assert response.status_code == status.HTTP_202_ACCEPTED
 
 
@@ -55,7 +55,7 @@ def test_get_movie():
     Test get book by ID
     :return: Book object
     """
-    response = client.get(f'{BASE_URL}/3')
+    response = client.get(f'{BASE_URL}/6')
     assert response.status_code == status.HTTP_200_OK
 
 
@@ -64,7 +64,7 @@ def test_get_movie_reviews():
     Get all reviews for a single book
     :return: List of reviews for a movie
     """
-    response = client.get(f'{BASE_URL}/reviews/3')
+    response = client.get(f'{BASE_URL}/reviews/6')
     assert response.status_code == status.HTTP_200_OK
 
 
@@ -73,7 +73,7 @@ def test_add_movie_review():
     Add review to a movie
     :return:  object
     """
-    response = client.post(f'{BASE_URL}/reviews/3', json=review_data)
+    response = client.post(f'{BASE_URL}/reviews/6', json=review_data)
     assert response.status_code == status.HTTP_201_CREATED
 
 
@@ -82,15 +82,15 @@ def test_avg_movie_rating():
     Get average rating of a movie
     :return: average rating
     """
-    response = client.get(f'{BASE_URL}/average-rating/3')
+    response = client.get(f'{BASE_URL}/average-rating/6')
     assert response.status_code == status.HTTP_200_OK
 
 
 def test_delete_movie():
-    response = client.delete(f'{BASE_URL}/7')
+    response = client.delete(f'{BASE_URL}/9')
     assert response.status_code == status.HTTP_204_NO_CONTENT
 
 
 def test_delete_movie_review():
-    response = client.delete(f'{BASE_URL}/reviews/11')
+    response = client.delete(f'{BASE_URL}/reviews/12')
     assert response.status_code == status.HTTP_204_NO_CONTENT
