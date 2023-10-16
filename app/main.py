664407cd2433ff31b066f876f8f49833d702795f
kenmoh <kenneth.aremoh@gmail.com>
2023-10-16 16:33:00 +0100

    update

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, status
+from fastapi.middleware.cors import CORSMiddleware
+
 
 from app.models import movie_model
 from app.database import engine
@@ -8,9 +10,24 @@ movie_model.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title='MoviesAPI', description='A Simple Movie Review REST API to demonstrate microservices')
 
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 async def health_check():
     return {'health_status': status.HTTP_200_OK}
+
+
 
 app.include_router(movie_routes.movie_router)
