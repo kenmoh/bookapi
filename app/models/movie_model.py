ef167914ee481eb3d276fdf4dda81914a7d70b80
kenmoh <kenneth.aremoh@gmail.com>
2023-12-04 12:41:17 +0100

    add type to movie model

--- app/models/movie_model.py
+++ app/models/movie_model.py
@@ -1,8 +1,15 @@
 from decimal import Decimal
+from enum import Enum
 from sqlalchemy import String, Integer, DECIMAL, ForeignKey, Float
 from sqlalchemy.orm import Mapped, relationship, mapped_column
 
 from app.database import Base
+
+
+class TypeEnum(str, Enum):
+    MOVIE = 'movie'
+    MUSIC = 'music'
+    BOOK = 'book'
 
 
 class Movie(Base):
@@ -14,7 +21,8 @@ class Movie(Base):
     description: Mapped[str] = mapped_column(String(100))
     cover_image_url: Mapped[str]
     casts: Mapped[str]
-    genre: Mapped[str]
+    genre: Mapped[str] = mapped_column(String, nullable=True)
+    type: Mapped[str] = mapped_column(String, nullable=True, default=TypeEnum.MOVIE)
     thriller: Mapped[str]
     average_rating: Mapped[float] = mapped_column(Float, nullable=True)
     reviews: Mapped[list["Movie"]] = relationship(
@@ -30,7 +38,7 @@ class Review(Base):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     author: Mapped[str]
-    comment: Mapped[str]
+    comment: Mapped[str] = mapped_column(String(400))
     rating: Mapped[int]
     ip_address: Mapped[str] = mapped_column(String(255))
     movie_id: Mapped[int] = mapped_column(ForeignKey("movies.id"), nullable=False)
